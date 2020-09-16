469b2e1ec7ae9c0eb4c6171586675bfcb8908688
wareset <git@wareset.com>
2020-09-16 13:10:37 +0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # svelte-use-mousetrap
-Use mousetrap in svelte
+
+Use [mousetrap](https://www.npmjs.com/package/mousetrap) in svelte. Everything is reactive. The 'destroy' works.
+
+## Installation
+
+```bash
+npm i svelte-use-mousetrap ## yarn add svelte-use-mousetrap
+```
+
+## Usage
+
+```svelte
+<script>
+/* App.svelte */
+import mousetrap from 'svelte-use-mousetrap';
+
+// run on 'command+q', 'ctrl+q'
+const fn1 = (e) => console.log('fn1', e);
+
+// run on 'shift+q'
+const fn2 = (e) => console.log('fn2', e);
+// run on 'shift+q'
+const fn3 = (e) => console.log('fn3', e);
+</script>
+
+<!-- simplest way -->
+<div use:mousetrap={['command+q', 'ctrl+q', fn1, 'shift+q' fn2, fn3]}>...</div>
+
+<!-- or exactly the same -->
+<div use:mousetrap={[
+  ['command+q', 'ctrl+q', fn1],
+  ['shift+q', fn2, fn3]
+]}>...</div>
+
+<!-- or exactly the same -->
+<div use:mousetrap={[
+  [['command+q', 'ctrl+q'], fn1],
+  ['shift+q', [fn2, fn3]]
+]}>...</div>
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,46 @@
+import Mousetrap from 'mousetrap';
+
+const isFunc = v => typeof v === 'function';
+const isArr = v => Array.isArray(v);
+
+const listen = (arr, mousetrap) => {
+  const result = [];
+  let v2, f1;
+  let i = 0;
+  arr.forEach((v1, k) => {
+    if (isArr(v1)) result.push(v1), ++i;
+    else {
+      (v2 = arr[k + 1]), (f1 = isFunc(v1));
+
+      if (!result[i]) result[i] = [[], []];
+      result[i][!f1 ? 0 : 1].push(v1);
+
+      if (v2 && f1 && !isFunc(v2)) ++i;
+    }
+  });
+
+  result.forEach(arr => {
+    if (arr.length !== 2) listen(arr, mousetrap);
+    else {
+      const types = arr[0],
+        fns = isArr(arr[1]) ? arr[1] : [arr[1]];
+
+      mousetrap.bind(types, (...a) => fns.forEach(fn => fn(...a)));
+    }
+  });
+};
+
+export default function use(node, arr) {
+  const mousetrap = new Mousetrap(window.document);
+
+  const destroy = () => {
+    mousetrap.reset();
+  };
+
+  const update = arr => {
+    destroy(), listen(arr, mousetrap);
+  };
+
+  update(arr);
+  return { update, destroy };
+}
